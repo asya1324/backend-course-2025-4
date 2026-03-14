e6775dd941bbc691cb49d78ecf45c979a66ec578
Asya bc2025-4 <Anastasiia.Samosuieva@lnu.edu.ua>
2026-03-14 17:19:42 +0200

    lab4 json read + filtering + xml response

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
 const { Command } = require('commander')
-const fs = require('fs')
+const fs = require('fs/promises')
 const http = require('http')
+const { XMLBuilder } = require('fast-xml-parser')
 
 const program = new Command()
 
 program
-  .requiredOption('-i, --input <path>', 'input file path')
-  .requiredOption('-H, --host <host>', 'server host')
-  .requiredOption('-p, --port <port>', 'server port')
+  .requiredOption('-i, --input <path>')
+  .requiredOption('-H, --host <host>')
+  .requiredOption('-p, --port <port>')
 
 program.parse()
-
 const options = program.opts()
 
-if (!fs.existsSync(options.input)) {
-  console.log('Cannot find input file')
-  process.exit(1)
-}
+const server = http.createServer(async (req, res) => {
+  try {
+    console.log("REQUEST:", req.url)
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Server is running')
+    const url = new URL(req.url, `http://${options.host}:${options.port}`)
+    const survivedQuery = url.searchParams.get('survived')
+    const ageQuery = url.searchParams.get('age')
+
+    const fileContent = await fs.readFile(options.input, 'utf-8')
+
+    let passengers = fileContent
+      .trim()
+      .split('\n')
+      .map(line => JSON.parse(line))
+
+    if (survivedQuery === 'true') {
+      passengers = passengers.filter(p => p.Survived == 1)
+    }
+
+    const xmlData = {
+      passengers: {
+        passenger: passengers.map(p => {
+          const obj = {
+            name: p.Name,
+            ticket: p.Ticket
+          }
+
+          if (ageQuery === 'true') {
+            obj.age = p.Age
+          }
+
+          return obj
+        })
+      }
+    }
+
+    const builder = new XMLBuilder({
+      format: true
+    })
+
+    const xml = builder.build(xmlData)
+
+    res.writeHead(200, { 'Content-Type': 'application/xml' })
+    res.end(xml)
+
+  } catch (err) {
+    console.log("ERROR:", err)
+    res.writeHead(500)
+    res.end('Server error')
+  }
 })
 
 server.listen(options.port, options.host, () => {
